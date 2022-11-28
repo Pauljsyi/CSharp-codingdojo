9c5639acbce44c2a91e0aff881b8c509e5d40fa4
pauljsyi <pauljsyi@gmail.com>
2022-11-28 13:18:54 -0800

    fundamentals 1

---
+++ CSharp-Practice/FundamentalsI/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+for(int i = 1; i < 256; i++) {
+    Console.WriteLine(i);
+}
+
+Random rand = new Random();
+for (int i =1; i<=5;i++){
+    Console.WriteLine(rand.Next(10,21));
+}
+Console.WriteLine("for loop: ");
+for (int i = 1; i < 101; i++) {
+        if (i%5==0 && i%3==0){
+            Console.WriteLine("FizzBuzz");
+        } else if (i%3 == 0) {
+            Console.WriteLine("Fizz");
+        } else if (i%5==0){
+            Console.WriteLine("Buzz");
+        } 
+}
+
+
+Console.WriteLine("While loop: ");
+int j = 1;
+while(j < 101) {
+    if (j%5==0 && j%3==0){
+        Console.WriteLine("FizzBuzz");
+    } else if (j%3 == 0) {
+        Console.WriteLine("Fizz");
+    } else if (j%5==0){
+        Console.WriteLine("Buzz");
+    } 
+    j++;
+}
