1c17f6a3c586872eb5c4283b0b2d7116f3a30b8a
pauljsyi <pauljsyi@gmail.com>
2022-11-28 12:07:48 -0800

    practice and moved console projects to practice

--- CSharp-Practice/HelloDotnet/Program.cs
+++ CSharp-Practice/HelloDotnet/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Random rand = new Random();
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(rand.Next());
+}
