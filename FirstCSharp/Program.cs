02c817e30fc9804794855af012723bc5e8af618c
pauljsyi <pauljsyi@gmail.com>
2022-11-28 10:22:44 -0800

    c# initialization

---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Data Types
+// What is a statically typed language?
+// You ahve to specify the type of variable or function you want to create
+// Javascript
+// var, let, const name = "Nichole";
+// C#
+string name = "Nichole";
+int number = 7;
+double dec = 3.14;
+float floatvalue = 1.2f;
+bool isTired = true;
+
+// Lists and Dictionaries
+// Array and List
+// Arrays are fixed length
+
+string[] groceryList = new string[4];
+// [null, null, null, null]
+
+string[] groceryList2 = {"Carrots", "Turkey", "Bread", "Milk"};
+
+groceryList[2] = "Ham";
+Console.WriteLine(groceryList);
+
